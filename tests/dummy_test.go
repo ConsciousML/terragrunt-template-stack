760aa99b2f71e58d19702f64d6a8ec6206f27dcf
Axel Mendoza <axel.mendoza@epita.fr>
2025-06-19 16:31:57 +0200

    add terratest for dummy module

---
+++ tests/dummy_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+    "os"
+    "testing"
+    "fmt"
+
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestDummyModule(t *testing.T) {
+    t.Parallel() // Enable parallel execution (if running with other tests)
+
+    tmpDir := os.TempDir()
+    testOutputDir := fmt.Sprintf("%s/dummy-%s", tmpDir, t.Name())
+    testContent := "Hello from Terratest!"
+
+    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+        TerraformDir: "../examples/tests/dummy",
+
+        Vars: map[string]interface{}{
+            "content":    testContent,
+            "output_dir": testOutputDir,
+        },
+    })
+
+    defer terraform.Destroy(t, terraformOptions)
+
+    terraform.InitAndApply(t, terraformOptions)
+
+    outputContent := terraform.Output(t, terraformOptions, "content")
+
+    assert.Equal(t, testContent, outputContent)
+}
